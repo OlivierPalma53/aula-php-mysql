3c16aba4454839ea3539d5a6c9bfae9a9b7546e8
Olivier Palma <olivierpalmasouza@gmail.com>
2017-08-07 16:48:57 -0300

    Trazendo listagem de usuarios do mysql

--- listar.php
+++ listar.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors',1);
+ini_set('display_startup_erros',1);
+error_reporting(E_ALL);
   require_once 'includes/header.php';
 ?>
 <div class="container">
@@ -19,6 +22,34 @@
         <th>Editar</th>
         <th>Excluir</th>
       </thead>
+      <?php
+
+        $conn = new mysqli("localhost", "root", "root", "cadastro");
+
+        if($conn->connect_error){
+          echo 'Erro: '.$conn->connect_error;
+        }
+
+        $sql = "SELECT * FROM gafanhotos";
+
+        $result = $conn->query($sql);
+
+        while ($row = $result->fetch_assoc()){
+          echo "<tr>";
+          echo "<td>".$row['id']."</td>";
+          echo "<td>".$row['nome']."</td>";
+          echo "<td>".$row['profissao']."</td>";
+          echo "<td>".$row['nascimento']."</td>";
+          echo "<td>".$row['sexo']."</td>";
+          echo "<td>".$row['peso']."</td>";
+          echo "<td>".$row['altura']."</td>";
+          echo "<td>".$row['nacionalidade']."</td>";
+          echo "<td><a href='form-edit.php?id=".$row['id']."'><button class='btn btn-warning'>Editar</button></a></td>";
+          echo "<td><a href='excluir.php?id='".$row['id']."><button class='btn btn-danger'>Excluir</button></a></td>";
+          echo "</tr>";
+        }
+
+      ?>
     </table>
   </div>
 </div>
