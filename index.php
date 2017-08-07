5c02d305f15fe5a7837a4d975684a22ead2bcc86
Olivier Palma <olivierpalmasouza@gmail.com>
2017-08-07 15:18:28 -0300

    Criando a index do projeto

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+  require_once 'includes/header.php';
+?>
+
+<div class="container">
+  <div class="page-header">
+    <h1>Sistema de gerenciamento de cadastro de alunos</h1>
+  </div>
+  <div class="jumbotron">
+    <p>
+      <strong>Seja bem vindo!</strong> esse e um sistema desenvolvido para as aulas de php com mysql onde vamos
+      conectar o php a um banco com cadastro de alunos chamados carinhosamente de gafanhtos.
+    </p>
+  </div>
+</div>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  </body>
+</html>
