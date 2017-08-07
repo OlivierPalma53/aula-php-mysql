4a454d41cafa8c183e871b3ce830792d90bf8ef8
Olivier Palma <olivierpalmasouza@gmail.com>
2017-08-07 15:28:36 -0300

    Criando a estrutura da pagina de listagem de alunos

---
+++ listar.php
@@ -0,0 +1,24 @@
+<?php
+  require_once 'includes/header.php';
+?>
+<div class="container">
+  <div class="page-header">
+    <h1>Listar Alunos</h1>
+  </div>
+  <div class="jumbotron">
+    <table class="table table-bordered table-striped table-hover table-responsive">
+      <thead>
+        <th>ID</th>
+        <th>Nome</th>
+        <th>Profissão</th>
+        <th>Data de Nascimento</th>
+        <th>Sexo</th>
+        <th>Peso</th>
+        <th>Altura</th>
+        <th>Nacionalidade</th>
+        <th>Editar</th>
+        <th>Excluir</th>
+      </thead>
+    </table>
+  </div>
+</div>
